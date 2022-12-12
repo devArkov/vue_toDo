8b8d0da8d90a1b8650b32e8a26b71f9a7e6cab44
Alexandr Korobov <devarko@sapo.pt>
2022-12-12 23:30:01 +0300

    Todo page

---
+++ src/views/TodoView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="todo">
+        <h1>Todo app</h1>
+    </div>    
+</template> 
+
+<script>
+    export default {
+        name: 'Todo',
+        data() {
+            return {
+                tasks
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
