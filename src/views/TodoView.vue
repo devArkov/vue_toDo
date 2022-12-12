a6cf703cc53f3f203856917953aa6c40aa9e0a2d
Alexandr Korobov <devarko@sapo.pt>
2022-12-12 23:43:01 +0300

    Tasks loop

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="todo">
         <h1>Todo app</h1>
+        <div class="task" v-for="(task, key) in tasks" :key="key">
+            <div class="title"><strong>Заголовок:</strong>{{ task.title }}</div>
+            <div class="text"><strong>Текст</strong>{{ task.text }}</div>
+        </div>
     </div>    
 </template> 
 
@@ -9,12 +13,35 @@
         name: 'Todo',
         data() {
             return {
-                tasks
+                tasks: [
+                    {id: 1, title: 'Задача 1', text: 'Подробно о задаче 1'},
+                    {id: 2, title: 'Задача 2', text: 'Подробно о задаче 2'},
+                    {id: 3, title: 'Задача 3', text: 'Подробно о задаче 3'},
+                    {id: 4, title: 'Задача 4', text: 'Подробно о задаче 4'},
+                ]
             }
         }
     }
 </script>
 
 <style scoped>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 
+    .todo {
+        padding: 15px;
+    }
+
+    .task {
+        margin-top: 15px;
+        padding: 15px;
+        border: 2px solid teal;
+    }
+
+    .text {
+        margin-top: 10px;
+    }
 </style>
